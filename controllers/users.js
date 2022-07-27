504fc7c7d393eb35aac79d1fec0e01681417a6dc
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-07-27 14:35:37 +0300

    check test

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,7 @@ module.exports.getUserById = (req, res) => {
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send(user))
     .catch((err) => errorMessage(err, req, res));
 };
 
