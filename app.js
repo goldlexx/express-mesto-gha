7d4407f7649936175dbac04ae55f8ecbf8caf778
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-08-09 14:04:34 +0300

    fix rev01

--- app.js
+++ app.js
@@ -3,22 +3,47 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const { errors } = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 const limiter = require('./utils/limiter');
 const { ErrorNotFound } = require('./errors/allErrors');
 const { auth } = require('./middlewares/auth');
-const routes = require('./routes/routes');
-
+const { createUser, login } = require('./controllers/users');
 const { handleError } = require('./middlewares/handleError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(routes);
 app.use(helmet());
 app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(
+        /https?:\/\/(www\.)?[a-zA-Z\d\-.]{1,}\.[a-z]{1,6}([/a-z0-9\-._~:?#[\]@!$&'()*+,;=]*)/,
+      ),
+    }),
+  }),
+  createUser,
+);
 
 app.use(auth);
 
