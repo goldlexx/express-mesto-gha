5cabff9ef9bbd134090742d48fa39fc8499f5298
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-08-09 14:25:15 +0300

    Fix rev01

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+const { Unauthorized } = require('../errors/allErrors');
 
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    throw new Unauthorized('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -12,7 +13,7 @@ module.exports.auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'some-secret-alohomora');
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    throw new Unauthorized('Необходима авторизация');
   }
 
   req.user = payload;
